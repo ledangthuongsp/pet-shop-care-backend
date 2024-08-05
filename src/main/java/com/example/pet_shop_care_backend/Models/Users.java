9436a267cda4b8a0adbb0395eeee440218fad8b6
Lê Đăng Thương <ledangthuongsp@gmail.com>
2024-08-05 23:33:39 +0700

    Update usser

--- src/main/java/com/example/pet_shop_care_backend/Models/Users.java
+++ src/main/java/com/example/pet_shop_care_backend/Models/Users.java
@@ -1,4 +1,18 @@
 package com.example.pet_shop_care_backend.Models;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
 public class Users {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String hashPassword;
+    private LocalDate dayOfBirth;
+    private String fullName;
+    private String phoneNumber;
 }
